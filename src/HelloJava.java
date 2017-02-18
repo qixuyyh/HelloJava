4993729581f781133cac48e52d45295321d5f3b5
qixuyyh <1246506093@qq.com>
2017-02-19 01:42:50 +0800

    首次提交到本地仓库

---
+++ src/HelloJava.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class HelloJava {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner scan=new Scanner(System.in);
+      System.out.println("请输入一个整数：");
+      int string=scan.nextInt();
+      int arr[]={7,10,1};
+      for (int i : arr) {
+		
+	}
+	}
+
+}
